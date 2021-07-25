09171f83d27a49a1acaf2d341ef7f022a73b8886
artemliger <artemliger@yandex.ru>
2021-07-25 03:45:13 +0300

    правки адаптива v1.2

--- public_html/app/themes/sage/resources/views/single.blade.php
+++ public_html/app/themes/sage/resources/views/single.blade.php
@@ -6,16 +6,20 @@
   <article class="blog-page">
     <h1 class="blog-page__title">{!! get_the_title() !!}</h1>
     <div class="blog-page__content">
-      <div class="blog-page__content-text">{!! $single_news['single_news_text1'] !!}</div>
-      <div class="blog-page__content-image">
-        @if($single_news['single_news_image'])
-          <img class="blog-page__content-image-img" src="{!! $single_news['single_news_image'] !!}"
-               alt="{!! get_the_title() !!}">
-        @else
-          <img class="blog-page__content-image-img" src="{!! get_the_post_thumbnail_url() !!}"
-               alt="{!! get_the_title() !!}">
-        @endif
-      </div>
+      @if($single_news['single_news_image'] || get_the_post_thumbnail_url())
+        <div class="blog-page__content-text">{!! $single_news['single_news_text1'] !!}</div>
+        <div class="blog-page__content-image">
+          @if($single_news['single_news_image'])
+            <img class="blog-page__content-image-img" src="{!! $single_news['single_news_image'] !!}"
+                 alt="{!! get_the_title() !!}">
+          @else
+            <img class="blog-page__content-image-img" src="{!! get_the_post_thumbnail_url() !!}"
+                 alt="{!! get_the_title() !!}">
+          @endif
+        </div>
+      @else
+        <div class="blog-page__content-text" style="max-width: 100%;">{!! $single_news['single_news_text1'] !!}</div>
+      @endif
     </div>
     <div class="blog-page__textarea">{!! $single_news['single_news_text2'] !!}</div>
     <div class="blog-page__footer">
